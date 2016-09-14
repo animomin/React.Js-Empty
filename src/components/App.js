821c4a71ac9480737bfadd9b20788fe34ced352c
animomin <animomin@daum.net>
2016-09-14 16:33:32 +0900

    Add Project files

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+    render(){
+
+        return (
+            <div>
+                <h1>Hello</h1>
+                <p>This is React Empty Project</p>
+            </div>
+        );
+    }
+}
+
+export default App;
