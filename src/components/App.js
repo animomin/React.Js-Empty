b8e9fc39646dd44a02747ef67d66eb3a65421ca9
animomin <animomin@daum.net>
2017-01-15 22:04:20 +0900

    new setting

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-class App extends React.Component {
-    render(){
+class App extends Component {
+    constructor(props){
+        super(props);
 
+        this.state ={
+            name : ''
+        };
+    }
+
+    render() {
         return (
             <div>
-                <h1>Hello</h1>
-                <p>This is React Empty Project</p>
+                <h1>Hello!!!</h1>
+                <p> welcome to react page!!!!! </p>
+                <p>{this.state.name}</p>
+                <button onClick={()=> {this.setState({name : 'animomin'});}}>Click Me</button>
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,35 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry : './src/index.js',
+    entry: './src/index.js',
 
-  output : {
-    path : __dirname + '/public/',
-    filename : 'bundle.js'
-  },
+    output: {
+        path: __dirname + '/public/',
+        filename: 'bundle.js'
+    },
 
-  devServer : {
-    inline : true,
-    port: 3000,
-    contentBase : __dirname + '/public/'
-  },
+    devServer: {
+        hot: true,
+        inline: true,
+        host: '0.0.0.0',
+        port: 4000,
+        contentBase: __dirname + '/public/',
+    },
 
-  module : {
-    loaders : [
-      {
-        test : /\.js$/,
-        loader : 'babel',
-        exclude : /node_modules/,
-        query : {
-          cacheDirectory : true,
-          presets : ['es2015', 'react']
-        }
-      }
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['react-hot','babel?' + JSON.stringify({
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react']
+                })],
+                exclude: /node_modules/                
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
     ]
-  }
-
-
 };
